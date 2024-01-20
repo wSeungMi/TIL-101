ff1fe16c4911e68fe71301b426ef8fa7e4c6ad37
wSeungMi <seungmi.dev@gmail.com>
2024-01-20 13:26:27 +0900

    Add: sbjs 1강 기록

---
+++ JavaScript/sbjs/ep01/main.js
@@ -0,0 +1,18 @@
+// document.body.innerHTML = "<h1>hello world</h1>";
+
+// const h1 = document.createElement("h1");
+// h1.innerText = "hello world";
+// h1.style.color = "red";
+// h1.style.fontWeight = "bold";
+// h1.classList;
+// document.body.appendChild(h1);
+// document.body.prepend(h1);
+
+document.querySelector("#app").innerHTML = `
+ <p>Hello1</p>
+ <p>Hello2</p>
+ <p>Hello3</p>
+`;
+
+// console.log(document.querySelector("p"));
+console.log(document.querySelectorAll("p"));
