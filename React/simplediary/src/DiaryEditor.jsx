6a6d3e707f76980b68a9bf33d69496d588d97989
wSeungMi <seungmi.dev@gmail.com>
2024-01-23 17:36:10 +0900

    Add: 사용자 입력 처리 연습

--- React/simplediary/src/DiaryEditor.jsx
+++ React/simplediary/src/DiaryEditor.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
+    // const [name, setName] = useState("");
+    // const [content, setContent] = useState("");
+
+    // 반복 구조로 사용되는 태그의 상태값을 한번에 사용하기
+    // 문제는 setState에 값을 넣을 때 하나하나 할당해주면, 개수가 많아질 때 비효율적이다.
+    // 그래서 스프레드 연산자를 사용!
     const [state, setState] = useState({
-        author: "",
+        name: "",
         content: "",
-        emotion: 1,
+        emotion: "5",
     });
 
+    // 위 방식도 개수가 많아지면 또 일일히 작성해줘야하는 단점이 존재한다.
+    // 더 좋은 방식으로 개선하려면?
     const handleChangeState = (e) => {
-        console.log(e.target.name);
-        console.log(e.target.value);
+        // console.log(e);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
         setState({
             ...state,
             [e.target.name]: e.target.value,
@@ -18,27 +27,19 @@ const DiaryEditor = () => {
 
     const handleSubmit = () => {
         console.log(state);
-        alert("저장 성공");
+        alert("저장 성공!");
     };
-    // const [author, setAuthor] = useState("");
-    // const [content, setContent] = useState("");
+
+    console.log(state);
 
     return (
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
                 <input
-                    name="author"
-                    value={state.author}
-                    // onChange={(e) => {
-                    //     console.log(e.target.value);
-                    //     console.log(e.target.name);
-                    //     setState({
-                    //         ...state,
-                    //         author: e.target.value,
-                    //         // content: state.content,
-                    //     });
-                    // }}
+                    name="name"
+                    type="text"
+                    value={state.name}
                     onChange={handleChangeState}
                 />
             </div>
@@ -46,14 +47,8 @@ const DiaryEditor = () => {
                 <textarea
                     name="content"
                     value={state.content}
-                    // onChange={(e) => {
-                    //     setState({
-                    //         author: state.author,
-                    //         content: e.target.value,
-                    //     });
-                    // }}
                     onChange={handleChangeState}
-                />
+                ></textarea>
             </div>
             <div>
                 오늘의 감정점수 :
@@ -62,11 +57,11 @@ const DiaryEditor = () => {
                     value={state.emotion}
                     onChange={handleChangeState}
                 >
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
+                    <option value={1}>😭</option>
+                    <option value={2}>😢</option>
+                    <option value={3}>🙂</option>
+                    <option value={4}>😀</option>
+                    <option value={5}>😆</option>
                 </select>
             </div>
             <div>
